2d067da32e72c058b269fdb0e2b2080127b67b0e
Sanjana Sahni <sanjanasahni1948@gmail.com>
2025-10-27 21:02:20 +0530

    Adding Lecture 1

---
+++ lec1.js
@@ -0,0 +1,82 @@
+// //  variable ko kaise banate hai
+
+// let name = "Harry";
+// let age= 20;
+// age=30;
+
+// console.log(name , age);
+
+// const account =1234;
+// console.log(account);
+
+// var a=10;
+// a=20;
+// console.log(a);
+//  if(true){
+//     var a =20;
+//  }
+//  function fun(){
+//     var c=30;
+//  }
+//  var b=10;
+//  console.log(c);
+
+//   primitive data type
+
+//   number, undefined, string, boolean, biginy, symbol
+
+// non primitive data type
+
+// let a=10;
+// let b=2.36;
+// console.log(typeof b);
+
+// let c="Strike is coming soon ."
+// let d="Sanjana";
+// console.log(c,d);
+
+//let login = true ;
+// let f= false;
+// console.log( typeof f);
+
+// let user;
+// const p=10;
+
+// console.log(user,p);
+
+// bigint
+// let num = 2345167858n;
+// console.log(typeof num);
+// // null
+// let weather = null;
+// console.log(typeof weather);
+// // symbol
+// const id1 = Symbol("id");
+// const id2= Symbol("id");
+// console.log(id2==id1);
+// let arr = [10,20,11,"Sanjana",true];
+
+// console.log(arr);
+// let use ={
+//     name:"Rohit",
+//     age:10,
+//     account:12341,
+//     category:'gen'
+// }
+// console.log(use);
+//  let s = function add(){
+//     // console.log("Hello");
+// }
+// console.log(s);
+let a=10;
+let b=a;
+ b=20;
+ console.log(a,b);
+let obj = {
+    name :"hary",
+    age: 20
+
+}
+let obj2 = obj;
+obj2.name="Carry";
+console.log(obj);
