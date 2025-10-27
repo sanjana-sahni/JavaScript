6e5dab12ecd4c5e2ca46546951697bc64e3cf964
Sanjana Sahni <sanjanasahni1948@gmail.com>
2025-10-27 21:10:00 +0530

    Adding lecture 4

---
+++ lec4.js
@@ -0,0 +1,42 @@
+/ //Numuber 
+ let a = 10;
+let b = 345.974;
+ let c = b.toFixed(2)
+console.log( c);
+
+console.log(b.toPrecision(3));
+console.log(b.toString())
+
+// let a = new Number(20);
+// let b = 20;
+// console.log(a==b);
+// let obj1 = {
+//     name: "Rohit"
+// }
+// // let obj2 = {
+// //     name:"Rohit"
+// // }
+// let obj2 =obj1;
+// console.log(obj1==obj2);
+
+// console.log(Boolean(10)); // ye normal hai means number hai
+// console.log(Boolean(null));  // ye object hai object ke andar sb true aate hai.  only null case false aayega kyo ki null bhi space lega vo bhi object hai
+
+
+// Non Primitive : Reference bases pe compare hote h..
+// Primitive : copy by value
+
+// let a = 10;
+// let b=a;
+// console.log(a==b);
+
+// console.log(Math.abs(-4));
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.SQRT2);
+// console.log(Math.ceil(5.4));
+// console.log(Math.floor(3.6));
+
+console.log(Math.floor(Math. random()*6 +1 ));
+// console.log(Math.random()*6);
+// (Math.random()*totalNumberofOutcomes)
